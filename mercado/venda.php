f39532612097a149f0cd36be186e109a8728ec0a
Bruno Lehmann <87442331+Lehmann-Bruno@users.noreply.github.com>
2024-11-19 20:06:30 -0300

    Update venda.php

--- mercado/venda.php
+++ mercado/venda.php
@@ -13,20 +13,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $data_venda = date('Y-m-d');
     
     // Recuperar preço do produto
-    $query_produto = "SELECT preco FROM PRODUTOS WHERE codigo = '$codigo'";
+    $query_produto = "SELECT preco, quantidade FROM PRODUTOS WHERE codigo = '$codigo'";
     $result_produto = mysqli_query($conn, $query_produto);
     $produto = mysqli_fetch_assoc($result_produto);
-    $valor = $produto['preco'] * $qtde_venda;
-    
-    // Inserir venda
-    $query_venda = "INSERT INTO VENDAS (codigo, qtde_venda, data_venda, valor) 
-                    VALUES ('$codigo', '$qtde_venda', '$data_venda', '$valor')";
-    $query_altera_quantidade = "UPDATE PRODUTOS SET quantidade - $qtde_venda WHERE codigo = $codigo";
-    if (mysqli_query($conn, $query_venda)) {
-        $query_altera_quantidade;
-        $msg = "Venda realizada com sucesso!";
+
+    if ($produto) {
+        $valor = $produto['preco'] * $qtde_venda;
+        $nova_quantidade = $produto['quantidade'] - $qtde_venda;
+
+        // Verificar se há estoque suficiente
+        if ($nova_quantidade >= 0) {
+            // Inserir venda
+            $query_venda = "INSERT INTO VENDAS (codigo, qtde_venda, data_venda, valor) 
+                            VALUES ('$codigo', '$qtde_venda', '$data_venda', '$valor')";
+            $query_altera_quantidade = "UPDATE PRODUTOS SET quantidade = '$nova_quantidade' WHERE codigo = '$codigo'";
+
+            // Executar as queries
+            if (mysqli_query($conn, $query_venda) && mysqli_query($conn, $query_altera_quantidade)) {
+                $msg = "Venda realizada com sucesso!";
+            } else {
+                $msg = "Erro ao registrar venda!";
+            }
+        } else {
+            $msg = "Estoque insuficiente!";
+        }
     } else {
-        $msg = "Erro ao registrar venda!";
+        $msg = "Produto não encontrado!";
     }
 }
 ?>
